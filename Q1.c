679620eb0dda1f9d6a615ac58040d9c95f6ab1a5
Pedro Henrique Sales Siquele <137921999+TheBestOFWord@users.noreply.github.com>
2026-03-25 18:18:34 -0300

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf(" == Biblioteca Universitaria ==\n");
+    printf("Sistema: BiblioSoft 2.0\n");
+    printf("Horario:8h00 - 22h00\n");
+    return 0;
+}
